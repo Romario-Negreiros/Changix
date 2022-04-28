89f43be77ada885426b81b2207bb2682e842f2fa
Romario Negreiros <nromario482@gmail.com>
2022-04-27 21:46:22 -0300

    feat: add next/head component

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Head from 'next/head'
+
 interface Props {
   children: React.ReactNode
 }
@@ -7,6 +9,10 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Changix</title>
+        <link rel="shortcut icon" href="images/logo.svg" />
+      </Head>
       {children}
     </>
   )
