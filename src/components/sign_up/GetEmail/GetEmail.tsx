bba323f95d05f44464750aab45facf450d02fdf3
Romario Negreiros <nromario482@gmail.com>
2022-04-29 20:30:13 -0300

    feat: create getEmail component

---
+++ src/components/sign_up/GetEmail/GetEmail.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import styles from './GetEmail.module.css'
+
+const GetEmail: React.FC = () => {
+  return (
+    <form className={styles.form}>
+      <p>
+        First, add your email address, a verification link will be sent after
+        you click the submit button. <br />
+        Check your inbox and click on the link received to verify the email
+        address. The email might take some minutes to arrive.
+      </p>
+      <input />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default GetEmail
