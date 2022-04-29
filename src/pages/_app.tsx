03fe3d0905e38d5782534aef731ca724e00f3ac7
Romario Negreiros <nromario482@gmail.com>
2022-04-28 21:38:23 -0300

    refactor: move page inside src folder

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Layout } from '../components'
+
+import '../styles/globals.css'
+import '../styles/variables.css'
+
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import type { NextPage } from 'next'
+
+import styles from '../styles/Landing.module.css'
+
+const Landing: NextPage = () => {
+  return (
+    <main className={styles.container}>
+
+    </main>
+  )
+}
+
+export default Landing
