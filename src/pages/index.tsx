ed6f22453c825b6a7e00ab4eac4ec964fad8f1b1
Romario Negreiros <nromario482@gmail.com>
2022-04-29 00:20:22 -0300

    chore: organize content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
-import type { NextPage } from 'next'
+// import Link from 'next/link'
 
 import styles from '../styles/Landing.module.css'
+
+import type { NextPage } from 'next'
 
 const Landing: NextPage = () => {
   return (
     <main className={styles.container}>
-
+      {/* <article className={styles.texts_container}>
+        <section>
+          <h1>Get rid of the old and find out the new</h1>
+          <p>
+            Here you can share the stuff under your bed for the last 10 years,
+            and find new things to change with people all over the world.
+          </p>
+        </section>
+        <section>
+          <Link href="/sign_in">
+            <a>Sign in now</a>
+          </Link>
+          <Link href="/home">
+            <a>Explore</a>
+          </Link>
+        </section>
+      </article>
+      <article className={styles.decorations_container}>
+        <div>Beatiful image soon...</div>
+      </article> */}
     </main>
   )
 }
