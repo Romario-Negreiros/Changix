d1f595197fd9df7446181dac56d3724e69030bba
Romario Negreiros <nromario482@gmail.com>
2022-05-02 13:53:52 -0300

    feat: add show/hide password

--- src/components/sign_up/Form/index.tsx
+++ src/components/sign_up/Form/index.tsx
@@ -4,14 +4,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import styles from '@styles/Form.module.css'
 
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import { faCamera, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+type TVisiblePwd = ('pwd' | 'confirm_pwd')[]
 
 const Form: React.FC = () => {
+  const [visiblePwd, setVisiblePwd] = React.useState<TVisiblePwd>([])
+
+  const handlePwdVisibility = (key: 'pwd' | 'confirm_pwd') => {
+    if (visiblePwd.includes(key)) {
+      setVisiblePwd(oldVisiblePwds =>
+        oldVisiblePwds.filter(visiblePwd => visiblePwd !== key)
+      )
+    } else {
+      setVisiblePwd([...visiblePwd, key])
+    }
+  }
+
   return (
     <form className={styles.form}>
       <section className={styles.file_input_container}>
         <label>
-          <FontAwesomeIcon icon={faCamera} color="#8661c1" height={25} width={25} />
+          <FontAwesomeIcon
+            icon={faCamera}
+            color="#8661c1"
+            height={25}
+            width={25}
+          />
           <input type="file" accept=".jpg,.jpeg,.png,.svg" />
         </label>
       </section>
@@ -25,11 +44,31 @@ const Form: React.FC = () => {
       </section>
       <section className={styles.input_container}>
         <label htmlFor="pwd">Password</label>
-        <input id="pwd" />
+        <div>
+          <FontAwesomeIcon
+            onClick={() => handlePwdVisibility('pwd')}
+            className={styles.icon}
+            icon={visiblePwd.includes('pwd') ? faEyeSlash : faEye}
+          />
+          <input
+            type={visiblePwd.includes('pwd') ? 'text' : 'password'}
+            id="pwd"
+          />
+        </div>
       </section>
       <section className={styles.input_container}>
         <label htmlFor="confirmd_pwd">Confirm Password</label>
-        <input id="confirm_pwd" />
+        <div>
+          <FontAwesomeIcon
+            onClick={() => handlePwdVisibility('confirm_pwd')}
+            className={styles.icon}
+            icon={visiblePwd.includes('confirm_pwd') ? faEyeSlash : faEye}
+          />
+          <input
+            type={visiblePwd.includes('confirm_pwd') ? 'text' : 'password'}
+            id="confirm_pwd"
+          />
+        </div>
       </section>
       <section className={styles.input_container}>
         <label htmlFor="country">Country</label>
