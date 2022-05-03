9ff2ba4299eb80e2385150a924afd5d4512471ca
Romario Negreiros <nromario482@gmail.com>
2022-05-03 12:35:54 -0300

    feat: add card and datalistinput components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
-import Image from 'next/image'
-import Link from 'next/link'
+import { DatalistInput, Card } from '../components'
 
 import styles from '@styles/Home.module.css'
 
@@ -11,32 +10,22 @@ const Home: NextPage = () => {
   return (
     <main className={styles.container}>
       <section className={styles.input_container}>
-        <input />
+        <DatalistInput />
+        <select>
+          <option value="" disabled selected>
+            Select a category
+          </option>
+          <option value="sports">Sports</option>
+          <option value="musical_instrument">Musical Instrument</option>
+        </select>
+        <button>Clear</button>
       </section>
       <article className={styles.results_container}>
-        <h1>Showing results for: auhsuahsuahsuash</h1>
+        <h1>Showing results for: auhsuahsuahsuash</h1> <br />
+        <h1>Using filter filter</h1>
         <ul>
           {new Array(50).fill(1).map((value, index) => (
-            <li key={value * index}>
-              <section className={styles.user_container}>
-                <div>
-                  <Image
-                    loading="lazy"
-                    src="/icons/logo.svg"
-                    width={50}
-                    height={50}
-                    alt="item.owner.name"
-                  />
-                </div>
-                <h2>item.owner.name</h2>
-              </section>
-              <section className={styles.item_image_container}>
-                <Image src="/images/landing_page.png" width="400px" height="200px" alt="landing" />
-              </section>
-              <Link href="/items/category/:id">
-                <a>Visit</a>
-              </Link>
-            </li>
+            <Card key={value * index} />
           ))}
         </ul>
       </article>
