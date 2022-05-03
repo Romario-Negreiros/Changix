87acd218103b1f03d552aec67ce18a46001e8f2d
Romario Negreiros <nromario482@gmail.com>
2022-05-03 15:00:10 -0300

    refactor: remove unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Layout } from '../components'
 
 import '../styles/globals.css'
 import '../styles/variables.css'
-import 'react-datalist-input/dist/styles.css'
 
 import type { AppProps } from 'next/app'
 
