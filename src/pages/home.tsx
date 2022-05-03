b1e432168dea25deef0a3f9cee8f055f5c11f1d7
Romario Negreiros <nromario482@gmail.com>
2022-05-03 15:01:56 -0300

    refactor: wrap input and select tags in a div to add label

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,16 +9,21 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <main className={styles.container}>
-      <section className={styles.input_container}>
-        <input placeholder="Search for item name" />
-        <select>
-          <option value="" disabled selected>
-            Select a category
-          </option>
-          <option value="sports">Sports</option>
-          <option value="musical_instrument">Musical Instrument</option>
-        </select>
-        <button>Clear</button>
+      <section>
+        <div className={styles.search_container}>
+          <label htmlFor="search">Search for item name</label>
+          <input id="search"/>
+        </div>
+        <div className={styles.filter_container}>
+          <label htmlFor="filter">Filter by category</label>
+          <select id="filter">
+            <option value="any" selected>
+              Any
+            </option>
+            <option value="sports">Sports</option>
+            <option value="musical_instrument">Musical Instrument</option>
+          </select>
+        </div>
       </section>
       <article className={styles.results_container}>
         <h1>
