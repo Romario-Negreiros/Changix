b9dcf7d31f110f1a58b93f37390080cfcbb30ddd
Romario Negreiros <nromario482@gmail.com>
2022-05-05 13:02:22 -0300

    feat: import react-responsive-carousel css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,13 @@ import { Layout } from '../components'
 
 import '../styles/globals.css'
 import '../styles/variables.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout >
+    <Layout>
       <Component {...pageProps} />
     </Layout>
   )
