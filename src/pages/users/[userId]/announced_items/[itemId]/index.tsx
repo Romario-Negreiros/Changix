a874df8297ffe5967a01918f00d1346df96abdc3
Romario Negreiros <nromario482@gmail.com>
2022-05-05 13:48:53 -0300

    feat: add styles and content

--- src/pages/users/[userId]/announced_items/[itemId]/index.tsx
+++ src/pages/users/[userId]/announced_items/[itemId]/index.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
 
+import formStyles from '@styles/Form.module.css'
+
 import type { NextPage } from 'next'
 
 const ManageItem: NextPage = () => {
   return (
-    <main>
-      oi
+    <main className="container">
+      <form className={formStyles.form}>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <label htmlFor="name">Name</label>
+          <input id="name" />
+        </section>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <label htmlFor="description">Description</label>
+          <textarea rows={4} id="description" />
+        </section>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <p className={formStyles.label_fallback}>Categories</p>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="sports" />
+            <label htmlFor="sports">Sports</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+        </section>
+        <button type="submit">Save changes</button>
+        <button type="button">Update images</button>
+      </form>
     </main>
   )
 }
