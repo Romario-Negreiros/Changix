290d83051f9f49932df48f4b916a49ff113221bd
Romario Negreiros <nromario482@gmail.com>
2022-05-06 15:06:33 -0300

    feat: add styles and content + add props interface

---
+++ src/components/ItemForm/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import { ClientOnlyPortal, ImagesUpdater } from '..'
+
+import formStyles from '@styles/components/Form.module.css'
+
+import type { SubmitBtn, FormFields } from './types'
+
+interface Props {
+  submitBtn: SubmitBtn
+  modalBtnTxt: string
+  defaultValues?: FormFields;
+
+}
+
+const ItemForm: React.FC<Props> = ({ submitBtn, modalBtnTxt, defaultValues }) => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false)
+
+  const setModalState = () => setIsModalOpen(!isModalOpen)
+
+  return (
+    <main className="container">
+      {isModalOpen && (
+        <ClientOnlyPortal selector="#modal">
+          <ImagesUpdater setModalState={setModalState} />
+        </ClientOnlyPortal>
+      )}
+      <form className={formStyles.form}>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <label htmlFor="name">Name</label>
+          <input id="name" />
+        </section>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <label htmlFor="description">Description</label>
+          <textarea rows={4} id="description" />
+        </section>
+        <section
+          className={`${formStyles.input_container} ${formStyles.full_width}`}
+        >
+          <p className={formStyles.label_fallback}>Categories</p>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="sports" />
+            <label htmlFor="sports">Sports</label>
+          </div>
+          <div className={formStyles.checkbox_wrapper}>
+            <input type="checkbox" id="musical_intruments" />
+            <label htmlFor="musical_instruments">Musical Instruments</label>
+          </div>
+        </section>
+        <button type="button" onClick={setModalState}>
+          {modalBtnTxt}
+        </button>
+        <button type="submit" onClick={submitBtn.onClick}>
+          {submitBtn.txt}
+        </button>
+      </form>
+    </main>
+  )
+}
+
+export default ItemForm
