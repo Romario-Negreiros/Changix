5484e7af3c64df358431932af723096790c0e25c
Romario Negreiros <nromario482@gmail.com>
2022-05-06 11:09:38 -0300

    refactor: remove change button + add legend title

--- src/components/modals/ImagesUpdater/index.tsx
+++ src/components/modals/ImagesUpdater/index.tsx
@@ -6,6 +6,7 @@ import styles from '@styles/modals/ImagesUpdater.module.css'
 import formStyles from '@styles/components/Form.module.css'
 
 import { faCamera, faClose } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
 
 interface Props {
   setModalState: () => void
@@ -15,26 +16,45 @@ const ImagesUpdater: React.FC<Props> = ({ setModalState }) => {
   return (
     <main className={styles.container}>
       <form className={formStyles.form}>
-      <div className="close_modal" onClick={setModalState}>
-        <FontAwesomeIcon icon={faClose} color="red" width={50} height={50} />
-      </div>
+        <h1>
+          Click on the image to change the current one, or on the camera to add
+          a new one.
+        </h1>
+        <div className="close_modal" onClick={setModalState}>
+          <FontAwesomeIcon icon={faClose} color="red" width={50} height={50} />
+        </div>
         {new Array(6).fill(1).map((value, index) => (
-        <section key={value * index} className={formStyles.file_input_container}>
-          <label className={formStyles.item_image_label}>
-            <FontAwesomeIcon
-              icon={faCamera}
-              color="#8661c1"
-              height={25}
-              width={25}
-            />
-            <input type="file" accept=".jpg,.jpeg,.png,.svg" />
-          </label>
-          <div className={styles.image_options_container}>
-            <button>Change</button>
-            <button style={{ backgroundColor: 'red' }}>Delete</button>
-            <button style={{ backgroundColor: 'orange' }}>Discard changes</button>
-          </div>
-        </section>
+          <section
+            key={value * index}
+            className={formStyles.file_input_container}
+          >
+            <label className={formStyles.item_image_label}>
+              {(value * index) % 2 === 0
+                ? (
+                <FontAwesomeIcon
+                  icon={faCamera}
+                  color="#8661c1"
+                  height={25}
+                  width={25}
+                />
+                  )
+                : (
+                <Image
+                  src="/images/453.jpg"
+                  width={300}
+                  height={200}
+                  alt="porra"
+                />
+                  )}
+              <input type="file" accept=".jpg,.jpeg,.png,.svg" />
+            </label>
+            <div className={styles.image_options_container}>
+              <button style={{ backgroundColor: 'red' }}>Delete</button>
+              <button style={{ backgroundColor: 'orange' }}>
+                Discard changes
+              </button>
+            </div>
+          </section>
         ))}
         <button type="submit">Save</button>
       </form>
