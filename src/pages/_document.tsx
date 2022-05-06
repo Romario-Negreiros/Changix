9cda60c264dab26abfdc3451845c03b075298478
Romario Negreiros <nromario482@gmail.com>
2022-05-06 10:11:02 -0300

    feat: create document file to add portal container

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <div id="modal" />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
