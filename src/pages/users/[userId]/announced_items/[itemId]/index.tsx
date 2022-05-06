6fcfe95c25fa7fc0f13fd561752a6384f0e21b26
Romario Negreiros <nromario482@gmail.com>
2022-05-06 15:10:02 -0300

    refactor: move html to component ItemForm and add ItemForm component with corresponding props

--- src/pages/users/[userId]/announced_items/[itemId]/index.tsx
+++ src/pages/users/[userId]/announced_items/[itemId]/index.tsx
@@ -1,70 +1,17 @@
 import React from 'react'
 
-import { ClientOnlyPortal, ImagesUpdater } from '../../../../../components'
-
-import formStyles from '@styles/components/Form.module.css'
+import { ItemForm } from '../../../../../components'
 
 import type { NextPage } from 'next'
 
+const submitBtn = {
+  txt: 'Save changes'
+}
+
 const ManageItem: NextPage = () => {
-  const [isModalOpen, setIsModalOpen] = React.useState(false)
-
-  const setModalState = () => setIsModalOpen(!isModalOpen)
-
   return (
     <main className="container">
-      {isModalOpen && (
-        <ClientOnlyPortal selector="#modal">
-          <ImagesUpdater setModalState={setModalState}/>
-        </ClientOnlyPortal>
-      )}
-      <form className={formStyles.form}>
-        <section
-          className={`${formStyles.input_container} ${formStyles.full_width}`}
-        >
-          <label htmlFor="name">Name</label>
-          <input id="name" />
-        </section>
-        <section
-          className={`${formStyles.input_container} ${formStyles.full_width}`}
-        >
-          <label htmlFor="description">Description</label>
-          <textarea rows={4} id="description" />
-        </section>
-        <section
-          className={`${formStyles.input_container} ${formStyles.full_width}`}
-        >
-          <p className={formStyles.label_fallback}>Categories</p>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="sports" />
-            <label htmlFor="sports">Sports</label>
-          </div>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="musical_intruments" />
-            <label htmlFor="musical_instruments">Musical Instruments</label>
-          </div>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="musical_intruments" />
-            <label htmlFor="musical_instruments">Musical Instruments</label>
-          </div>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="musical_intruments" />
-            <label htmlFor="musical_instruments">Musical Instruments</label>
-          </div>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="musical_intruments" />
-            <label htmlFor="musical_instruments">Musical Instruments</label>
-          </div>
-          <div className={formStyles.checkbox_wrapper}>
-            <input type="checkbox" id="musical_intruments" />
-            <label htmlFor="musical_instruments">Musical Instruments</label>
-          </div>
-        </section>
-        <button type="submit">Save changes</button>
-        <button type="button" onClick={setModalState}>
-          Update images
-        </button>
-      </form>
+      <ItemForm submitBtn={submitBtn} modalBtnTxt="Update images" />
     </main>
   )
 }
