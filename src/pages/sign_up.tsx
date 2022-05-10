02d56bed1387f94fb1ce22840276e7dcca374e89
Romario Negreiros <nromario482@gmail.com>
2022-05-10 00:26:24 -0300

    feat: fetching countries list using getStaticProps and pass it as props to the Form component

--- src/pages/sign_up.tsx
+++ src/pages/sign_up.tsx
@@ -2,14 +2,49 @@ import React from 'react'
 
 import { Form } from '../components'
 
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+import type { Country } from '@app/types/auth'
 
-const SignUp: NextPage = () => {
+interface FetchedCountry {
+  name: {
+    common: string
+  }
+  cca2: string
+  idd: {
+    root: string
+    suffixes: string[]
+  }
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('https://restcountries.com/v3.1/all')
+  const data = await response.json()
+
+  const countries = data.map((country: FetchedCountry) => {
+    return {
+      name: country.name.common,
+      alpha2Code: country.cca2,
+      callingCode: country.idd.root + country.idd.suffixes?.join()
+    }
+  })
+
+  return {
+    props: {
+      countries
+    }
+  }
+}
+
+interface Props {
+  countries: Country[]
+}
+
+const SignUp: NextPage<Props> = ({ countries }) => {
+  console.log(countries)
   return (
     <main className="container">
-      {/* GetEmail = 1 */}
+      <Form countries={countries} />
       {/* VerfiyEmailMessage = 2 */}
-      <Form />
     </main>
   )
 }
