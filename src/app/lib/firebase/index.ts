5e7c8af16af493185664690a4d3ac77951dec1f7
Romario Negreiros <nromario482@gmail.com>
2022-05-10 18:06:04 -0300

    refactor: rename env variables

--- src/app/lib/firebase/index.ts
+++ src/app/lib/firebase/index.ts
@@ -10,12 +10,12 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_SB,
-  messagingSenderId: process.env.REACT_APP_MS_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_SB,
+  messagingSenderId: process.env.NEXT_PUBLIC_MS_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
