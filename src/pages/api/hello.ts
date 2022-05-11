14d43f50fcc2c24eef66485f2de5b1d8160c30af
Romario Negreiros <nromario482@gmail.com>
2022-05-11 15:49:37 -0300

    refactor: rewrite using arrow function

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-}
+export const handler = (req: NextApiRequest, res: NextApiResponse) => {}
