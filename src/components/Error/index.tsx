4257c291e52ed5562577f39b390ee97db2ffeacd
Romario Negreiros <nromario482@gmail.com>
2022-05-10 23:00:53 -0300

    feat: add content and component props

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Lottie from 'lottie-react'
+
+import errorAnimation from '@public/animations/error.json'
+
+import styles from '@styles/components/Error.module.css'
+
+interface Props {
+  title: string
+  error: string
+  btn: {
+    handleClick: () => void
+    text: string
+  }
+  animation?: any
+}
+
+const Error: React.FC<Props> = ({ title, error, btn, animation }) => {
+  return (
+    <main className={styles.container}>
+      <section>
+        <h1>{title}</h1>
+        <div style={animation ? { width: '55%' } : undefined}>
+          <Lottie animationData={animation ?? errorAnimation} loop />
+        </div>
+        <p>{error}</p>
+        <button onClick={btn.handleClick}>{btn.text}</button>
+      </section>
+    </main>
+  )
+}
+
+export default Error
