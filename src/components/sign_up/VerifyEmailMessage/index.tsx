8400e26a1f40bb545fb8a87e66dbff48831fb3ae
Romario Negreiros <nromario482@gmail.com>
2022-05-12 12:35:42 -0300

    feat: add user prop and condition to check if user was verified inside useEffect

--- src/components/sign_up/VerifyEmailMessage/index.tsx
+++ src/components/sign_up/VerifyEmailMessage/index.tsx
@@ -1,47 +1,39 @@
 import React from 'react'
 
-import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+import Lottie from 'lottie-react'
 
 import styles from '@styles/components/VerifyEmailMessage.module.css'
 
 import Waiting from '@public/animations/waiting.json'
-import Success from '@public/animations/success-tick.json'
 
-const VerifyEmailMessage: React.FC = () => {
-  const [verified, setVerified] = React.useState(false)
-  const lottieRef = React.useRef<LottieRefCurrentProps>(null)
+import { User } from 'firebase/auth'
+import { useRouter } from 'next/router'
+
+interface Props {
+  user: User
+}
+
+const VerifyEmailMessage: React.FC<Props> = ({ user }) => {
+  const router = useRouter()
 
   React.useEffect(() => {
-    if (!verified) {
-      setTimeout(() => setVerified(true), 5000)
-    } else {
-      // Stops the animation to leave it in the screen before it disappears
-      const durationInSeconds = lottieRef.current?.getDuration() // 3.016666...s
-      if (durationInSeconds) {
-        const durationInMilliseconds = (durationInSeconds - 0.5) * 1000
-        setTimeout(() => lottieRef.current?.pause(), durationInMilliseconds)
-      }
+    if (user.emailVerified) {
+      router.push('/home')
     }
-  }, [verified])
+  }, [user.emailVerified, router])
 
   return (
-    <section className={styles.container}>
-      <p>
-        Now check your email&apos;s inbox and click the link to verify your
-        email!
-      </p>
-      {verified
-        ? (
-        <div>
-          <Lottie lottieRef={lottieRef} animationData={Success} style={{ width: '350px' }} />
-        </div>
-          )
-        : (
+    <main className="container">
+      <section className={styles.container}>
+        <p>
+          Now check your email&apos;s inbox and click the link to verify your
+          email!
+        </p>
         <div>
           <Lottie animationData={Waiting} loop />
         </div>
-          )}
-    </section>
+      </section>
+    </main>
   )
 }
 
