68c154a13f3c8e506243244355fbab6c7f5068fa
Romario Negreiros <nromario482@gmail.com>
2022-05-12 17:58:25 -0300

    feat: add link to home page

--- src/components/sign_up/VerifyEmailMessage/index.tsx
+++ src/components/sign_up/VerifyEmailMessage/index.tsx
@@ -1,37 +1,27 @@
 import React from 'react'
 
+import Link from 'next/link'
+
 import Lottie from 'lottie-react'
 
 import styles from '@styles/components/VerifyEmailMessage.module.css'
 
 import Waiting from '@public/animations/waiting.json'
 
-import { User } from 'firebase/auth'
-import { useRouter } from 'next/router'
-
-interface Props {
-  user: User
-}
-
-const VerifyEmailMessage: React.FC<Props> = ({ user }) => {
-  const router = useRouter()
-
-  React.useEffect(() => {
-    if (user.emailVerified) {
-      router.push('/home')
-    }
-  }, [user.emailVerified, router])
-
+const VerifyEmailMessage: React.FC = () => {
   return (
     <main className="container">
       <section className={styles.container}>
         <p>
           Now check your email&apos;s inbox and click the link to verify your
-          email!
+          email! <br />
         </p>
         <div>
           <Lottie animationData={Waiting} loop />
         </div>
+        <Link href="/home">
+          <a>Already verified?</a>
+        </Link>
       </section>
     </main>
   )
