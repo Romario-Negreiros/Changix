a2907f36babf4be27fadf309748ee3645c3abd7f
Romario Negreiros <nromario482@gmail.com>
2022-05-12 18:01:11 -0300

    feat: add authContext provider component + use useProvideAuth hook to set authContext value + add useEffect with onAuthStateChanged listener to set user's state + add cleanup function in useEffect to unsubscribe onAuthStateChanged listener

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,36 @@
 import React from 'react'
 
-import { AuthProvider } from '@utils/services/auth'
+import { useProvideAuth } from '@utils/hooks'
+import firebase from '@app/lib/firebase'
+
 import { Layout } from '../components'
 
 import '../styles/globals.css'
 import '../styles/variables.css'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+import authContext from '@app/contexts/authContext'
+
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const auth = useProvideAuth()
+
+  React.useEffect(() => {
+    const unsubscribe = firebase.auth.instance.onAuthStateChanged(user => {
+      auth.setUser(user)
+    })
+
+    return () => unsubscribe()
+  }, [auth])
+
   return (
-    <AuthProvider>
+    <authContext.Provider value={auth}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </AuthProvider>
+    </authContext.Provider>
   )
 }
 
