437ed6bf7136297f426bedf04d27283487f88a52
Romario Negreiros <nromario482@gmail.com>
2022-05-13 18:18:06 -0300

    feat: add react hook form + create onSubmit function + handle form states (error, loader and success components)

--- src/pages/forgot_password.tsx
+++ src/pages/forgot_password.tsx
@@ -1,21 +1,74 @@
 import React from 'react'
 
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { useAuth } from '@utils/hooks'
+
+import { Error as ErrorComponent, Loader, Success } from '../components'
+
 import formStyles from '@styles/components/Form.module.css'
 
 import type { NextPage } from 'next'
 
+interface FormField {
+  email: string
+}
+
 const ForgotPassword: NextPage = () => {
+  const [error, setError] = React.useState('')
+  const [isLoaded, setIsLoaded] = React.useState(true)
+  const [success, setSuccess] = React.useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormField>()
+  const { sendPasswordResetEmail } = useAuth()
+
+  const onSubmit: SubmitHandler<FormField> = async ({ email }) => {
+    try {
+      setIsLoaded(false)
+      await sendPasswordResetEmail(email)
+      setSuccess(true)
+    } catch (err) {
+      if (err instanceof Error) setError('')
+    } finally {
+      setIsLoaded(true)
+    }
+  }
+
+  if (!isLoaded) {
+    return <Loader />
+  } else if (error) {
+    return (
+      <ErrorComponent
+        title="Oooops"
+        error={error}
+        btn={{ handleClick: () => setError(''), text: 'Dismiss' }}
+      />
+    )
+  } else if (success) {
+    return (
+      <Success
+        title="Link succesfully sent!"
+        message="Check your email to access the link"
+      />
+    )
+  }
   return (
     <main className="container">
-      <form className={formStyles.form}>
-        <h1>
-          Enter your email address to receive a reset password link
-        </h1>
+      <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
+        <h1>Enter your email address to receive a reset password link</h1>
         <section className={formStyles.input_container}>
           <label htmlFor="email">Email</label>
-          <input id="email"/>
+          <input
+            {...register('email', { required: 'Email is required' })}
+            id="email"
+          />
+          <p className={formStyles.error}>{errors.email?.message}</p>
         </section>
-        <button type="submit" style={{ maxWidth: '400px' }}>Send link</button>
+        <button type="submit" style={{ maxWidth: '400px' }}>
+          Send link
+        </button>
       </form>
     </main>
   )
