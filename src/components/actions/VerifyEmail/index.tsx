e2feac25c68f1af200a1dfa80fcb15307b57f6a7
Romario Negreiros <nromario482@gmail.com>
2022-05-13 18:15:27 -0300

    refactor: remove redirect mechanism and use Success component

--- src/components/actions/VerifyEmail/index.tsx
+++ src/components/actions/VerifyEmail/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import styles from '@styles/components/VerifyEmail.module.css'
-
 import { useRouter } from 'next/router'
 import { useAuth } from '@utils/hooks'
 
-import { Error as ErrorComponent, Loader } from '../../'
+import { Error as ErrorComponent, Loader, Success } from '../../'
 
 interface Props {
   oobCode: string
@@ -14,7 +12,6 @@ interface Props {
 const VerifyEmail: React.FC<Props> = ({ oobCode }) => {
   const [error, setError] = React.useState('')
   const [hasVerified, setHasVerified] = React.useState(false)
-  const [redirectTimer, setRedirectTimer] = React.useState(5)
   const { user, signOut, verifyEmailAddress } = useAuth()
   const { push } = useRouter()
 
@@ -29,19 +26,8 @@ const VerifyEmail: React.FC<Props> = ({ oobCode }) => {
           if (err instanceof Error) setError(err.message)
         }
       })()
-    } else {
-      setTimeout(() => setRedirectTimer(redirectTimer - 1), 1000)
-      if (redirectTimer === 0) push('/sign_in')
     }
-  }, [
-    hasVerified,
-    user,
-    redirectTimer,
-    push,
-    verifyEmailAddress,
-    oobCode,
-    signOut
-  ])
+  }, [hasVerified, user, push, verifyEmailAddress, oobCode, signOut])
 
   if (error) {
     return (
@@ -53,12 +39,10 @@ const VerifyEmail: React.FC<Props> = ({ oobCode }) => {
     )
   } else if (hasVerified) {
     return (
-      <main className="container">
-        <div className={styles.wrapper}>
-          <h1>Email succesfully verified!</h1>
-          <p>Redirecting you to sign in page in {redirectTimer} seconds</p>
-        </div>
-      </main>
+      <Success
+        title="Email succesfully verified!"
+        redirect={{ url: '/sign_in', page: 'sign in' }}
+      />
     )
   }
   return <Loader />
