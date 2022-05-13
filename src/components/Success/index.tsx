cac8bfe862cee74e15eb83704cc37e46e9947474
Romario Negreiros <nromario482@gmail.com>
2022-05-13 18:16:33 -0300

    feat: add styles and content + add optional redirect mechanism

---
+++ src/components/Success/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { useRouter } from 'next/router'
+
+import styles from 'Success.module.css'
+
+interface Props {
+  title: string
+  message?: string
+  redirect?: {
+    url: string
+    page: string
+  }
+}
+
+const Success: React.FC<Props> = ({ title, message, redirect }) => {
+  const [redirectTimer, setRedirectTimer] = React.useState(5)
+  const { push } = useRouter()
+
+  React.useEffect(() => {
+    if (redirect) {
+      if (redirectTimer === 0) {
+        push(redirect?.url)
+      }
+      setTimeout(() => setRedirectTimer(redirectTimer - 1), 1000)
+    }
+  }, [redirectTimer, push, redirect])
+
+  return (
+    <main className="container">
+      <div className={styles.wrapper}>
+        <h1>{title}</h1>
+        <p>
+          {message || `Redirecting you to ${redirect?.page} page in ${redirectTimer} seconds`}
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default Success
