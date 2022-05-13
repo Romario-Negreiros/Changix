2e7cb1c92beab78a1146f33d3cc168de4a9bee40
Romario Negreiros <nromario482@gmail.com>
2022-05-13 00:27:19 -0300

    feat: create onSubmit function + handle form state (loader and error components)

--- src/components/sign_up/Form/index.tsx
+++ src/components/sign_up/Form/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 import { handlePwdVisibility } from '@utils/handlers'
+import { useAuth } from '@utils/hooks'
+
+import { Error as ErrorComponent, Loader } from '../../'
 
 import PhoneInput from 'react-phone-number-input/input'
 
@@ -35,6 +38,9 @@ const Form: React.FC<Props> = ({ countries }) => {
   const [visiblePwds, setVisiblePwds] = React.useState<VisiblePwds[]>([])
   const [imgPreview, setImgPreview] = React.useState('')
   const [country, setCountry] = React.useState<Country | undefined>()
+  const [error, setError] = React.useState('')
+  const [isLoaded, setIsLoaded] = React.useState(true)
+  const { createUserWithEmailAndPassword, sendEmailVerification } = useAuth()
   const {
     register,
     handleSubmit,
@@ -65,7 +71,19 @@ const Form: React.FC<Props> = ({ countries }) => {
   }, [setValue])
 
   const onSubmit: SubmitHandler<FormFields> = async data => {
-    console.log(data)
+    try {
+      if (data.confirmPwd === data.pwd) {
+        setIsLoaded(false)
+        const user = await createUserWithEmailAndPassword(data.email, data.pwd)
+        await sendEmailVerification(user)
+      } else {
+        throw new Error('Confirm pwd and aoskasoaks')
+      }
+    } catch (err) {
+      if (err instanceof Error) setError(err.message)
+    } finally {
+      setIsLoaded(true)
+    }
   }
 
   const handleCountrySelection = (event: React.FormEvent<HTMLInputElement>) => {
@@ -98,139 +116,152 @@ const Form: React.FC<Props> = ({ countries }) => {
     }
   }
 
+  if (error) {
+    return (
+      <ErrorComponent
+        title="Oooops"
+        error={error}
+        btn={{ handleClick: () => setError(''), text: 'Dismiss' }}
+      />
+    )
+  } else if (!isLoaded) {
+    return <Loader />
+  }
   return (
-    <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
-      <section className={formStyles.file_input_container}>
-        <label>
-          {imgPreview
-            ? (
-            <Image
-              src={imgPreview}
-              width={100}
-              height={75}
-              alt="Image preview"
+    <main className="container">
+      <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
+        <section className={formStyles.file_input_container}>
+          <label>
+            {imgPreview
+              ? (
+              <Image
+                src={imgPreview}
+                width={100}
+                height={75}
+                alt="Image preview"
+              />
+                )
+              : (
+              <FontAwesomeIcon
+                icon={faCamera}
+                color="#8661c1"
+                height={25}
+                width={25}
+              />
+                )}
+            <input
+              type="file"
+              accept=".jpg,.jpeg,.png,.svg"
+              {...register('picture')}
+              onChange={handleFileSelection}
             />
-              )
-            : (
+          </label>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^.+[@]{1}[aA-zZ]+[.]{1}.+$/,
+                message: 'Invalid email format. example@mail.com'
+              }
+            })}
+          />
+          <p className={formStyles.error}>{errors.email?.message}</p>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            {...register('name', {
+              required: 'Name is required'
+            })}
+          />
+          <p className={formStyles.error}>{errors.name?.message}</p>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="pwd">Password</label>
+          <div>
             <FontAwesomeIcon
-              icon={faCamera}
-              color="#8661c1"
-              height={25}
-              width={25}
+              onClick={() =>
+                handlePwdVisibility('pwd', visiblePwds, setVisiblePwds)
+              }
+              className={formStyles.icon}
+              icon={visiblePwds.includes('pwd') ? faEyeSlash : faEye}
             />
-              )}
-          <input
-            type="file"
-            accept=".jpg,.jpeg,.png,.svg"
-            {...register('picture')}
-            onChange={handleFileSelection}
-          />
-        </label>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          {...register('email', {
-            required: 'Email is required',
-            pattern: {
-              value: /^.+[@]{1}[aA-zZ]+[.]{1}.+$/,
-              message: 'Invalid email format. example@mail.com'
-            }
-          })}
-        />
-        <p className={formStyles.error}>{errors.email?.message}</p>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          {...register('name', {
-            required: 'Name is required'
-          })}
-        />
-        <p className={formStyles.error}>{errors.name?.message}</p>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="pwd">Password</label>
-        <div>
-          <FontAwesomeIcon
-            onClick={() =>
-              handlePwdVisibility('pwd', visiblePwds, setVisiblePwds)
-            }
-            className={formStyles.icon}
-            icon={visiblePwds.includes('pwd') ? faEyeSlash : faEye}
-          />
-          <input
-            type={visiblePwds.includes('pwd') ? 'text' : 'password'}
-            id="pwd"
-            {...register('pwd', {
-              required: 'Password is required',
-              ...pwdValidationRules
-            })}
-          />
-        </div>
-        <p className={formStyles.error}>{errors.pwd?.message}</p>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="confirmd_pwd">Confirm Password</label>
-        <div>
-          <FontAwesomeIcon
-            onClick={() =>
-              handlePwdVisibility('confirm_pwd', visiblePwds, setVisiblePwds)
-            }
-            className={formStyles.icon}
-            icon={visiblePwds.includes('confirm_pwd') ? faEyeSlash : faEye}
-          />
-          <input
-            type={visiblePwds.includes('confirm_pwd') ? 'text' : 'password'}
-            id="confirm_pwd"
-            {...register('confirmPwd', {
-              required: 'You must repeat your password',
-              ...pwdValidationRules
-            })}
-          />
-        </div>
-        <p className={formStyles.error}>{errors.confirmPwd?.message}</p>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="country">Country</label>
-        <input
-          id="country"
-          list="countries"
-          {...register('country', {
-            required: 'Country is required'
-          })}
-          onBlur={handleCountrySelection}
-        />
-        <datalist id="countries">
-          {countries.map(country => (
-            <option key={country.name} value={country.name} />
-          ))}
-        </datalist>
-        <p className={formStyles.error}>{errors.country?.message}</p>
-      </section>
-      <section className={formStyles.input_container}>
-        <label htmlFor="phone_number">Phone number</label>
-        <Controller
-          name="phoneNumber"
-          control={control}
-          rules={{
-            required: 'Phone number is required'
-          }}
-          render={({ field: { onChange, value } }) => (
-            <PhoneInput
-              id="phone_input"
-              country={country?.alpha2Code}
-              onChange={onChange}
-              value={value}
+            <input
+              type={visiblePwds.includes('pwd') ? 'text' : 'password'}
+              id="pwd"
+              {...register('pwd', {
+                required: 'Password is required',
+                ...pwdValidationRules
+              })}
             />
-          )}
-        />
-        <p className={formStyles.error}>{errors.phoneNumber?.message}</p>
-      </section>
-      <button type="submit">Submit</button>
-    </form>
+          </div>
+          <p className={formStyles.error}>{errors.pwd?.message}</p>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="confirmd_pwd">Confirm Password</label>
+          <div>
+            <FontAwesomeIcon
+              onClick={() =>
+                handlePwdVisibility('confirm_pwd', visiblePwds, setVisiblePwds)
+              }
+              className={formStyles.icon}
+              icon={visiblePwds.includes('confirm_pwd') ? faEyeSlash : faEye}
+            />
+            <input
+              type={visiblePwds.includes('confirm_pwd') ? 'text' : 'password'}
+              id="confirm_pwd"
+              {...register('confirmPwd', {
+                required: 'You must repeat your password',
+                ...pwdValidationRules
+              })}
+            />
+          </div>
+          <p className={formStyles.error}>{errors.confirmPwd?.message}</p>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="country">Country</label>
+          <input
+            id="country"
+            list="countries"
+            {...register('country', {
+              required: 'Country is required'
+            })}
+            onBlur={handleCountrySelection}
+          />
+          <datalist id="countries">
+            {countries.map(country => (
+              <option key={country.name} value={country.name} />
+            ))}
+          </datalist>
+          <p className={formStyles.error}>{errors.country?.message}</p>
+        </section>
+        <section className={formStyles.input_container}>
+          <label htmlFor="phone_number">Phone number</label>
+          <Controller
+            name="phoneNumber"
+            control={control}
+            rules={{
+              required: 'Phone number is required'
+            }}
+            render={({ field: { onChange, value } }) => (
+              <PhoneInput
+                id="phone_input"
+                country={country?.alpha2Code}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+          <p className={formStyles.error}>{errors.phoneNumber?.message}</p>
+        </section>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   )
 }
 
