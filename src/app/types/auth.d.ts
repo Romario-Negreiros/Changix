b5d20fff87101c9b4b71dd7a641fb1adaf1c8b9b
Romario Negreiros <nromario482@gmail.com>
2022-05-13 00:22:04 -0300

    feat: add signOut, signInWithEmailAndPassword and verifyEmailAddress props to AuthContext interface

--- src/app/types/auth.d.ts
+++ src/app/types/auth.d.ts
@@ -4,26 +4,29 @@ import { CountryCode } from 'libphonenumber-js/types'
 import { User } from 'firebase/auth'
 
 export interface Country {
-  name: string;
-  alpha2Code: CountryCode;
+  name: string
+  alpha2Code: CountryCode
 }
 
 export interface FormFields {
-  picture: FileList;
-  email: string;
-  name: string;
-  pwd: string;
-  confirmPwd: string;
-  country: string;
-  phoneNumber: string;
+  picture: FileList
+  email: string
+  name: string
+  pwd: string
+  confirmPwd: string
+  country: string
+  phoneNumber: string
 }
 
-export interface UserProfile extends Omit<FormFields, 'email' | 'pwd' | 'confirmPwd'> {
-}
+export interface UserProfile
+  extends Omit<FormFields, 'email' | 'pwd' | 'confirmPwd'> {}
 
 export interface AuthContext {
   user: User | null
   setUser: React.Dispatch<React.SetStateAction<User | null>>
   createUserWithEmailAndPassword: (email: string, pwd: string) => Promise<User>
   sendEmailVerification: (user: User | null) => Promise<void>
+  signOut: () => Promise<void>
+  signInWithEmailAndPassword: (email: string, pwd: string) => Promise<void>
+  verifyEmailAddress: (oobCode: string) => Promise<void>
 }
