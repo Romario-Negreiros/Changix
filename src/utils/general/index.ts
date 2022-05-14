24fdbafbc80c1c602cb59a902765ad0869f280d8
Romario Negreiros <nromario482@gmail.com>
2022-05-13 23:16:50 -0300

    feat: create function to join error messages and the action where the error came out to use in error handlers

---
+++ src/utils/general/index.ts
@@ -0,0 +1,1 @@
+export { default as joinErrMsgAndAction } from './joinErrMsgAndAction'
---
+++ src/utils/general/joinErrMsgAndAction.ts
@@ -0,0 +1,5 @@
+const joinErrMsgAndAction = (errMsg: string, action: string) => {
+  return `msg: ${errMsg} \n action: ${action}`
+}
+
+export default joinErrMsgAndAction
