ee001fa1c21a3e23d27df0d515f56c6f3a8dd6be
Romario Negreiros <nromario482@gmail.com>
2022-05-14 21:54:54 -0300

    feat: handle errors in onAuthStateChanged listener

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import { useProvideAuth } from '@utils/hooks'
+import { handleAuthError } from '@utils/handlers'
 import firebase from '@app/lib/firebase'
 
-import { Layout } from '../components'
+import { Layout, Error } from '../components'
 
 import '../styles/globals.css'
 import '../styles/variables.css'
@@ -15,12 +16,18 @@ import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  const [error, setError] = React.useState('')
   const auth = useProvideAuth()
 
   React.useEffect(() => {
-    const unsubscribe = firebase.auth.instance.onAuthStateChanged(user => {
-      auth.setUser(user)
-    })
+    const unsubscribe = firebase.auth.instance.onAuthStateChanged(
+      user => {
+        auth.setUser(user)
+      },
+      error => {
+        handleAuthError(error as unknown, 'Auth state listener', setError)
+      }
+    )
 
     return () => unsubscribe()
   }, [auth])
@@ -28,7 +35,17 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <authContext.Provider value={auth}>
       <Layout>
-        <Component {...pageProps} />
+        {error
+          ? (
+          <Error
+            title="Oooops"
+            error={error}
+            btn={{ handleClick: () => setError(''), text: 'Dismiss' }}
+          />
+            )
+          : (
+          <Component {...pageProps} />
+            )}
       </Layout>
     </authContext.Provider>
   )
