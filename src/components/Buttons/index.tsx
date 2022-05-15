7ac86e4d1b0d3948ab4829adc8367b5fdf1f3a75
Romario Negreiros <nromario482@gmail.com>
2022-05-14 21:45:53 -0300

    feat: add Buttons component to render buttons of user profile page

---
+++ src/components/Buttons/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+type Modals = 'change_pwd' | 'delete_acc'
+
+interface Props {
+  isEditing: boolean
+  setFormState: () => void
+  setModalState: () => void
+  setModalOpened: (modalOpened: Modals | null) => void
+}
+
+const Buttons: React.FC<Props> = ({
+  isEditing,
+  setFormState,
+  setModalOpened,
+  setModalState
+}) => {
+  return (
+    <>
+      {!isEditing && (
+        <button type="button" onClick={setFormState}>
+          Update informations
+        </button>
+      )}
+      {isEditing && <button type="submit">Save</button>}
+      <button
+        type="button"
+        onClick={() => {
+          setModalState()
+          setModalOpened('change_pwd')
+        }}
+      >
+        Change password
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setModalState()
+          setModalOpened('delete_acc')
+        }}
+      >
+        Delete password
+      </button>
+    </>
+  )
+}
+
+export default Buttons
