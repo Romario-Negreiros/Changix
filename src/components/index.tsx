1e0f40d5d951de340b1f617b96c0005d14e27b64
Romario Negreiros <nromario482@gmail.com>
2022-05-14 21:55:20 -0300

    refactor: recreate layout component and move header component inside layout folder

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-export { default as Header } from './Header'
+export { default as Layout } from './Layout'
 export { default as VerifyEmailMessage } from './VerifyEmailMessage'
 export { default as SignUpForm } from './SignUpForm'
 export { default as Card } from './Card'
