1569ead12e92690f50c004f9c360d78144f50717
Romario Negreiros <nromario482@gmail.com>
2022-05-19 13:20:59 -0300

    fetch items in getServerSideProps + handle errors (error component) + render card components with items informations

--- src/pages/users/[userId]/announced_items/index.tsx
+++ src/pages/users/[userId]/announced_items/index.tsx
@@ -1,24 +1,98 @@
 import React from 'react'
 
-import { Card } from 'src/components'
+import { useRouter } from 'next/router'
+import { useFirestore } from '@utils/hooks'
 
-import type { NextPage } from 'next'
+import { Card, Error } from 'src/components'
+
+import Sleeping from '@public/animations/sleeping.json'
+
+import type { GetServerSideProps, NextPage } from 'next'
+import type { Item } from '@app/types/item'
 
 const containerStyles = {
   display: 'flex',
   alignItems: 'stretch',
   justifyContent: 'center',
   flexFlow: 'row wrap',
-  gap: '2rem',
+  gap: '0rem',
   padding: '1rem',
   marginTop: '2rem'
 }
 
-const AnnouncedItems: NextPage = () => {
+interface Props {
+  items: Item[]
+  userId: string
+  serverSideError?: string
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const userId = context.params?.userId
+
+  if (!userId) {
+    return {
+      props: {
+        serverSideError: 'Invalid id'
+      }
+    }
+  }
+
+  const firestoreFunctions = useFirestore
+  const { getDocs } = firestoreFunctions()
+
+  const results = await getDocs(['announced'], ['ownerId', '==', userId])
+  if (results.empty) {
+    return {
+      props: {
+        serverSideError: 'No announces created'
+      }
+    }
+  }
+  const items = results.docs.map(doc => {
+    const data = doc.data() as Item
+    return {
+      id: doc.id,
+      name: data.name,
+      description: data.description
+    }
+  })
+
+  return {
+    props: {
+      items,
+      userId
+    }
+  }
+}
+
+const AnnouncedItems: NextPage<Props> = ({
+  items,
+  userId,
+  serverSideError
+}) => {
+  const { back } = useRouter()
+
+  if (serverSideError) {
+    return (
+      <Error
+        title="zzzzz"
+        error={serverSideError}
+        animation={Sleeping}
+        width="55%"
+        btn={{ handleClick: () => back(), text: 'Go back' }}
+      />
+    )
+  }
   return (
     <main style={containerStyles}>
-      {new Array(50).fill(1).map((value, index) => (
-        <Card key={value * index} linkHref={`/users/5/announced_items/${index}`}/>
+      {items.map(item => (
+        <>
+          <Card
+            key={item.id}
+            item={item}
+            linkHref={`/users/${userId}/announced_items/${item.id}`}
+          />
+        </>
       ))}
     </main>
   )
