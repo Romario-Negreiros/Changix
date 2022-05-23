5560442248ec051f6b04f0fd27fbea8f6242286a
Romario Negreiros <nromario482@gmail.com>
2022-05-23 18:13:20 -0300

    feat: export new hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useProvideAuth } from './useProvideAuth'
 export { default as useAuth } from './useAuth'
 export { default as useFirestore } from './useFirestore'
+export { default as useSearch } from './useSearch'
