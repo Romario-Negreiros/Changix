4661e2b4bd8e3883b734b554579295712ac6878b
Romario Negreiros <nromario482@gmail.com>
2022-05-23 18:11:26 -0300

    feat: add react hook form + handle query state (homeQueryState and loader components) + use getServerSideProps to render initial list of item + add handleQuery function and use debouncer to trigger a query some seconds after the user complete an search action (typing in the input or selecting an option from the filter list) + add listener to inputs changes and unsubscribe from listener when Home page unmounts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-// import { Card } from '../components'
+import { useForm } from 'react-hook-form'
+import { debounce } from '@utils/general'
+import { useFirestore, useSearch } from '@utils/hooks'
+
+import { HomeQueryState, Loader } from '../components'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -8,26 +12,125 @@ import styles from '@styles/pages/Home.module.css'
 
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { Item, CategoriesEnum } from '@app/types/item'
 
-const selectOptions = ['Any', 'Sports', 'Musical Instruments']
+interface Props {
+  initialItems: Item[]
+}
 
-const Home: NextPage = () => {
+interface Fields {
+  search: string
+  filter: CategoriesEnum
+}
+
+const selectOptions = ['', 'Sports', 'Musical Instruments']
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const firestoreFunctions = useFirestore
+  const { getDocs } = firestoreFunctions()
+
+  const results = await getDocs(['announced'], ['ownerId', '!=', ''], 25)
+  const initialItems: Item[] = []
+  results.forEach(result => {
+    if (result.exists()) {
+      initialItems.push({
+        id: result.id,
+        ...(result.data() as Omit<Item, 'id'>)
+      })
+    }
+  })
+
+  return {
+    props: {
+      initialItems
+    }
+  }
+}
+
+const Home: NextPage<Props> = ({ initialItems }) => {
+  const [error, setError] = React.useState('')
+  const [loadersState, setLoadersState] = React.useState({
+    isLoadingSearchQuery: false,
+    isLoadingScrollQuery: false
+  })
+  const [items, setItems] = React.useState<Item[]>(initialItems)
+  const { getDocs } = useFirestore()
+  const { search, filter, searchAndFilter } = useSearch()
+  const { register, watch, setValue } = useForm<Fields>()
+  const watchSearch = watch('search')
+  const watchFilter = watch('filter')
+
+  React.useEffect(() => {
+    if (!items.length) {
+      setError('No results found!')
+    }
+  }, [items])
+
+  const handleQuery = async (value: Partial<Fields>) => {
+    try {
+      setLoadersState({
+        isLoadingScrollQuery: false,
+        isLoadingSearchQuery: true
+      })
+      if (value.search) {
+        if (value.filter) {
+          const results = await searchAndFilter(value.search, value.filter)
+          setItems(results)
+          return
+        }
+        const results = await search(value.search)
+        setItems(results)
+        return
+      } else if (value.filter) {
+        const results = await filter(value.filter)
+        setItems(results)
+        console.log(value.filter)
+        return
+      }
+    } catch (err) {
+      if (err instanceof Error) setError(err.message)
+    } finally {
+      setLoadersState({
+        isLoadingScrollQuery: false,
+        isLoadingSearchQuery: false
+      })
+    }
+  }
+
+  const handleQueryWithDebounce = debounce((value: Partial<Fields>) =>
+    handleQuery(value)
+  )
+
+  React.useEffect(() => {
+    const subscription = watch(async value => handleQueryWithDebounce(value))
+
+    return () => subscription.unsubscribe()
+  }, [watch, getDocs, search, filter, searchAndFilter, handleQueryWithDebounce])
+
   return (
     <main className={styles.container}>
       <section>
         <div className={styles.search_container}>
           <label htmlFor="search">Search for item name</label>
           <div className={styles.input_wrapper}>
-            <input id="search" />
+            <input id="search" {...register('search')} />
             <div className={styles.clear_input}>
-              <FontAwesomeIcon icon={faClose} color="#8661c1" width={25} height={25} />
+              {watchSearch && (
+                <FontAwesomeIcon
+                  onClick={() => setValue('search', '')}
+                  icon={faClose}
+                  color="#8661c1"
+                  width={25}
+                  height={25}
+                />
+              )}
             </div>
           </div>
         </div>
         <div className={styles.filter_container}>
           <label htmlFor="filter">Filter by category</label>
-          <select id="filter">
+          <select id="filter" placeholder="None" {...register('filter')}>
             {selectOptions.map((option, index) => (
               <option key={index} value={option.toLowerCase()}>
                 {option}
@@ -38,14 +141,19 @@ const Home: NextPage = () => {
       </section>
       <article className={styles.results_container}>
         <h1>
-          Showing results for: auhsuahsuahsuash <br />
-          Using filter filter
+          Showing results for: {watchSearch} <br />
+          Using filter: {watchFilter}
         </h1>
-        <ul>
-          {/* {new Array(50).fill(1).map((value, index) => (
-            <Card key={value * index} linkHref="/items/:category/:itemId" />
-          ))} */}
-        </ul>
+        <HomeQueryState
+          error={error}
+          isLoadingSearchQuery={loadersState.isLoadingSearchQuery}
+          items={items}
+        />
+        {loadersState.isLoadingScrollQuery && (
+          <div className={styles.scrollContainer}>
+            <Loader notFullScreen />
+          </div>
+        )}
       </article>
     </main>
   )
