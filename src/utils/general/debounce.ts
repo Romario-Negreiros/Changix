dd24c196f4587749522ffea8ae1865df608b058b
Romario Negreiros <nromario482@gmail.com>
2022-05-23 18:12:46 -0300

    feat: create debounce function and add types

---
+++ src/utils/general/debounce.ts
@@ -0,0 +1,14 @@
+const debounce = (func: (...args: any[]) => any, timeout = 300) => {
+  let timer: NodeJS.Timeout | undefined
+
+  return (...args: any[]) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args as [])
+    }, timeout)
+  }
+}
+
+export default debounce
