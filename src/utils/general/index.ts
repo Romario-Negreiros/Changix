fc5fb7c0fac490d7863c9c673fade7623b92c8a3
Romario Negreiros <nromario482@gmail.com>
2022-05-23 18:13:00 -0300

    feat: export new function

--- src/utils/general/index.ts
+++ src/utils/general/index.ts
@@ -1,2 +1,3 @@
 export { default as joinErrMsgAndAction } from './joinErrMsgAndAction'
 export { default as joinPathSegmentsWithSlash } from './joinPathSegmentsWithSlash'
+export { default as debounce } from './debounce'
