871e545e80b3db34f42c00ddb45bbcee4fa71ff0
Romario Negreiros <nromario482@gmail.com>
2022-05-23 18:14:34 -0300

    feat: create search, filter and searchAndFilter functions using useFirestore hook and export those insisde an object

---
+++ src/utils/hooks/useSearch.ts
@@ -0,0 +1,75 @@
+import { useFirestore } from '.'
+
+import type { Item, CategoriesEnum } from '@app/types/item'
+
+const useSearch = () => {
+  const { getDocs } = useFirestore()
+
+  const search = async (searchValue: string) => {
+    const results = await getDocs(['announced'], ['ownerId', '!=', ''], 100)
+    const filteredResults: Item[] = []
+    results.forEach(doc => {
+      const item = doc.data() as Omit<Item, 'id'>
+      if (filteredResults.length < 24) {
+        if (item.name.toLowerCase().includes(searchValue.toLowerCase())) {
+          filteredResults.push({
+            id: doc.id,
+            ...item
+          })
+        }
+      }
+    })
+    return filteredResults
+  }
+
+  const filter = async (filterValue: CategoriesEnum) => {
+    const results = await getDocs(
+      ['announced'],
+      ['category', '==', filterValue],
+      100
+    )
+    const filteredResults: Item[] = []
+    results.forEach(doc => {
+      const item = doc.data() as Omit<Item, 'id'>
+      if (filteredResults.length < 24) {
+        filteredResults.push({
+          id: doc.id,
+          ...item
+        })
+      }
+    })
+    return filteredResults
+  }
+
+  const searchAndFilter = async (
+    searchValue: string,
+    filterValue: CategoriesEnum
+  ) => {
+    const results = await getDocs(
+      ['announced'],
+      ['category', '==', filterValue],
+      100
+    )
+    const filteredResults: Item[] = []
+    results.forEach(doc => {
+      const item = doc.data() as Omit<Item, 'id'>
+      if (filteredResults.length < 24) {
+        if (item.name.toLowerCase().includes(searchValue.toLowerCase())) {
+          filteredResults.push({
+            id: doc.id,
+            ...item
+          })
+        }
+      }
+    })
+    return filteredResults
+  }
+
+  return {
+    search,
+    searchAndFilter,
+    filter
+  }
+}
+
+export default useSearch
