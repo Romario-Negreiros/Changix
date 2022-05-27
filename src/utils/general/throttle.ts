6027ed7f651da2d4f1573aa577ce1b0c47d68d14
Romario Negreiros <nromario482@gmail.com>
2022-05-27 16:00:56 -0300

    feat: add isScrollQuery argument to ensure that the function will be called at least when the scroll reaches the bottom of the page

--- src/utils/general/throttle.ts
+++ src/utils/general/throttle.ts
@@ -1,8 +1,19 @@
-const throttle = (func: (...args: any[]) => any, wait = 500) => {
+const throttle = (func: (...args: any[]) => any, wait: number, isScrollQuery?: boolean) => {
   let lastTime = 0
 
   return (...args: any[]) => {
     const now = Date.now()
+    if (isScrollQuery) {
+      const documentHeight = document.body.scrollHeight
+      const currentScroll = window.scrollY + window.innerHeight
+      if (currentScroll === documentHeight) {
+        func(args)
+        lastTime = now
+      } else if (now - lastTime >= wait) {
+        func(args)
+        lastTime = now
+      }
+    }
     if (now - lastTime >= wait) {
       func(args)
       lastTime = now
