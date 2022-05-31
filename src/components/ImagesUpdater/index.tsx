6157844dc5d2727d995e3f199f706382fd8b7b6c
Romario Negreiros <nromario482@gmail.com>
2022-05-31 17:50:49 -0300

    feat: add react hook form + add conditions to render either a default image, a image preview or a camera icon + create getInputRegisterName to return the input's name based in the index argument + create clearAllFields and clearOneField functions

--- src/components/ImagesUpdater/index.tsx
+++ src/components/ImagesUpdater/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { handleFileSelection } from '@utils/handlers'
+
 import { CloseModal } from '..'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,53 +13,119 @@ import { faCamera } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
 
 import type { SharedProps } from '@app/types/modals'
+import type { FormFields } from '@app/types/item'
+import { UseFormRegister, UseFormResetField } from 'react-hook-form'
 
-interface Props extends SharedProps {}
+interface Props extends SharedProps {
+  register: UseFormRegister<FormFields>
+  resetField: UseFormResetField<FormFields>
+  imagesPreviews: any
+  setImagesPreviews: (callback: (oldImagesPreviews: any) => void) => void
+  defaultImagesValues: string[]
+}
 
-const ImagesUpdater: React.FC<Props> = ({ setModalState }) => {
+const ImagesUpdater: React.FC<Props> = ({
+  setModalState,
+  register,
+  resetField,
+  imagesPreviews,
+  setImagesPreviews,
+  defaultImagesValues
+}) => {
+  const getInputRegisterName = (index: number) => {
+    switch (index) {
+      case 0:
+        return 'image0'
+      case 1:
+        return 'image1'
+      case 2:
+        return 'image2'
+      case 3:
+        return 'image3'
+      case 4:
+        return 'image4'
+      default:
+        return 'image5'
+    }
+  }
+
+  const clearOne = (index: number) => {
+    setImagesPreviews(oldImagesPreviews => {
+      return {
+        ...oldImagesPreviews,
+        [getInputRegisterName(index)]: ''
+      }
+    })
+    resetField(getInputRegisterName(index))
+  }
+
+  const clearAll = () => {
+    setImagesPreviews(() => {
+      return {}
+    })
+    for (let img = 0; img < 6; img++) {
+      resetField(getInputRegisterName(img))
+    }
+  }
+
   return (
     <main className="modal_container">
-      <form className={formStyles.form}>
+      <section className={formStyles.form}>
         <h1>
           Click on the image to change the current one, or on the camera to add
           a new one.
         </h1>
         <CloseModal setModalState={setModalState} />
-        {new Array(6).fill(1).map((value, index) => (
+        {new Array(6).fill(1).map((_, index) => (
           <section
-            key={value * index}
+            key={`input#${index}`}
             className={formStyles.file_input_container}
           >
             <label className={formStyles.item_image_label}>
-              {(value * index) % 2 === 0
-                ? (
-                <FontAwesomeIcon
-                  icon={faCamera}
-                  color="#8661c1"
-                  height={25}
-                  width={25}
-                />
-                  )
-                : (
+              {defaultImagesValues[index] &&
+                !imagesPreviews[`image${index}`] && (
+                  <Image
+                    src={defaultImagesValues[index]}
+                    width={300}
+                    height={200}
+                    alt="item"
+                  />
+              )}
+              {imagesPreviews[`image${index}`] && (
                 <Image
-                  src="/images/453.jpg"
+                  src={imagesPreviews[`image${index}`]}
                   width={300}
                   height={200}
-                  alt="porra"
+                  alt={`img preview ${index}`}
                 />
-                  )}
-              <input type="file" accept="image/*" />
+              )}
+              {!defaultImagesValues[index] &&
+                !imagesPreviews[`image${index}`] && (
+                  <FontAwesomeIcon
+                    icon={faCamera}
+                    color="#8661c1"
+                    height={25}
+                    width={25}
+                  />
+              )}
+              <input
+                type="file"
+                accept="image/*"
+                {...register(getInputRegisterName(index), {
+                  onChange: e =>
+                    handleFileSelection(e, undefined, index, setImagesPreviews)
+                })}
+              />
             </label>
             <div className={styles.image_options_container}>
-              <button style={{ backgroundColor: 'red' }}>Delete</button>
-              <button style={{ backgroundColor: 'orange' }}>
-                Discard changes
-              </button>
+              <button onClick={() => clearOne(index)}type="button" style={{ backgroundColor: 'red' }}>Delete</button>
             </div>
           </section>
         ))}
-        <button type="submit">Save</button>
-      </form>
+        <button onClick={clearAll} type="button" style={{ backgroundColor: 'orange' }}>
+          Discard changes
+        </button>
+      </section>
     </main>
   )
 }
