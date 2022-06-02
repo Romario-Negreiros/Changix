99af1f59bb16c449488110514ccae362ffc52a01
Romario Negreiros <nromario482@gmail.com>
2022-06-02 13:19:32 -0300

    refactor: add optional chaining before accessing length prop of images array + add markUndefineds argument and push undefined to images array if it is true

--- src/utils/general/getAllImagesInAnArray.ts
+++ src/utils/general/getAllImagesInAnArray.ts
@@ -1,12 +1,14 @@
 import type { FormFields } from '@app/types/item'
 
-const getAllImagesInAnArray = (data: FormFields) => {
-  const images: File[] = []
+const getAllImagesInAnArray = (data: FormFields, markUndefineds?: boolean) => {
+  const images: (File | undefined)[] = []
   const propsWithImage = ['image0', 'image1', 'image2', 'image3', 'image4', 'image5']
   for (const prop in data) {
     if (propsWithImage.includes(prop)) {
-      if (data[prop as keyof FormFields].length) {
+      if (data[prop as keyof FormFields]?.length) {
         images.push(data[prop as keyof FormFields][0] as File)
+      } else {
+        if (markUndefineds) images.push(undefined)
       }
     }
   }
